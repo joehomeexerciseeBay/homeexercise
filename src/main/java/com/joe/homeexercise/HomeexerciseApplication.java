66ef816106487711a80f2abf3e5388589914b7cf
joe-sagayaraj <60450298+joe-sagayaraj@users.noreply.github.com>
2021-02-25 15:14:02 -0800

    H2 DB
    Setting up H2 DB and security config to access it.

--- src/main/java/com/joe/homeexercise/HomeexerciseApplication.java
+++ src/main/java/com/joe/homeexercise/HomeexerciseApplication.java
@@ -2,6 +2,10 @@ package com.joe.homeexercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class HomeexerciseApplication {
@@ -9,6 +13,16 @@ public class HomeexerciseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HomeexerciseApplication.class, args);
 		System.out.println("Hello eBay from Joe");
+	}
+	
+	@Configuration
+	public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+    public void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+    }
 	}
 
 }
