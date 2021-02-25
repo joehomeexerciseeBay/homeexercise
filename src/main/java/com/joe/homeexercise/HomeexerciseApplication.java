9df114b06cf64c227c1edf948ff705ff4499b8b5
joe-sagayaraj <60450298+joe-sagayaraj@users.noreply.github.com>
2021-02-25 14:29:22 -0800

    Initial commit

---
+++ src/main/java/com/joe/homeexercise/HomeexerciseApplication.java
@@ -0,0 +1,14 @@
+package com.joe.homeexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeexerciseApplication.class, args);
+		System.out.println("Hello eBay");
+	}
+
+}
---
+++ src/test/java/com/joe/homeexercise/HomeexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joe.homeexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
