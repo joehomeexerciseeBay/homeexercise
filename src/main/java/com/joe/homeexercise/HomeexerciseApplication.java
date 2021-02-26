89496154e2fea7724a451f0bca7996a012c0a04d
joe-sagayaraj <60450298+joe-sagayaraj@users.noreply.github.com>
2021-02-25 19:55:25 -0800

    WIP

--- src/main/java/com/joe/homeexercise/HomeexerciseApplication.java
+++ src/main/java/com/joe/homeexercise/HomeexerciseApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
--- src/main/java/com/joe/homeexercise/model/SellerEntity.java
+++ src/main/java/com/joe/homeexercise/model/SellerEntity.java
@@ -4,14 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
