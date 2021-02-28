9da7eb2b37bb54329812e08ec714263c76605fc1
joe-sagayaraj <60450298+joe-sagayaraj@users.noreply.github.com>
2021-02-28 14:16:48 -0800

    WIP

--- src/main/java/com/joe/homeexercise/service/ItemEligibilityService.java
+++ src/main/java/com/joe/homeexercise/service/ItemEligibilityService.java
@@ -4,9 +4,7 @@
 package com.joe.homeexercise.service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
